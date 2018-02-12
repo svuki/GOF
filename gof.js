634405d65645836a95a554278a02164e9e8728ee
Stefan Vukicevic <stefanvuk17@gmail.com>
2018-02-12 02:41:32 -0800

    Add Decorate_gof which handles updating of view.

--- gof.js
+++ gof.js
@@ -1,38 +1,28 @@
-var canvas_width  = 900;
-var canvas_height = 500;
-var pixel_size    = 10;
-var the_canvas = document.getElementById('gof-canvas');
-var the_ctx = the_canvas.getContext('2d');
-var the_gof = new Gof(classic_rule);
 
 
-
-
-function draw_cell(ctx, val, i, j) {
-	let to_canv_coord = (i) => i * pixel_size;
-	let color = val ? 'black' : 'white';
-	ctx.fillStyle = color;
-	ctx.fillRect(to_canv_coord(i), to_canv_coord(j), 10, 10);
-
-}
 
 function apply_rule(rule_fn, tdarray){
   return tdarray.map ( rule_fn );
 }
 
-function Gof(rule) {
+function Gof(cols, rows, rule=classic_rule) {
+	this.generation = 0;
 	this.rule = rule;
-	this.cells = new TDArray(90, 50);
-	this.next  = function () { this.cells = apply_rule(this.rule, this.cells); };
+	this.cells = new TDArray(cols, rows);
+	this.next  = function () { 
+		this.cells = apply_rule(this.rule, this.cells); 
+		this.generation++
+	};
 	//TODO: what to do for toggle of more than one possible val?
 	this.toggle = (i, j) => {
 		var val = this.cells.at(i,j) ? 0 : 1
 		this.cells.set(i,j,val);
-		draw_cell(the_ctx, val, i, j);
-	}
-	this.show = function() {this.cells.for_each ((val, i, j) => draw_cell(the_ctx, val, i, j));} 
-	this.clear = () => {this.cells = this.cells.map( () => 0 );
-			    this.show()}	
+		return val;
+	};
+	this.clear = () => {
+		this.cells = this.cells.map( () => 0 );
+		this.generate = 0;
+	};	
 } 
 
 function classic_rule(val, i, j, td){
@@ -48,18 +38,73 @@ function classic_rule(val, i, j, td){
         return 0;
 } 
 
+const colors = {0 : 'white', 1: 'black' };
 
-function to_gof_coord(x) { 
-	return Math.floor( (x - 10) / pixel_size);
+function draw_cell(ctx, cell_size, val, i, j) {
+	let to_canv_coord = (i) => i * cell_size;
+	let color = colors[val];
+	ctx.fillStyle = color;
+	ctx.fillRect(to_canv_coord(i), to_canv_coord(j), 10, 10);
+
+}
+function canvas_to_gof(canvas, cell_size=10) {
+    //creates a game of life representation with corresponding dimensions
+    //to the CANVAS and CELL_SIZE
+    return new Gof(canvas.width / cell_size, canvas.height / cell_size);
 }
 
-the_canvas.addEventListener('click', 
-	    		(e) => {the_gof.toggle(to_gof_coord (e.clientX), to_gof_coord (e.clientY) )} )
-var rate_cont = new SpeedController(n=1000, () => { the_gof.next(); the_gof.show()})
-function toggle_go() {rate_cont.toggle()}
-function reset() {the_gof.clear()}
+function Decorated_gof(canvas, cell_size=10) {
+    
+    const saved = {};
+    const ctx = canvas.getContext('2d');
+    const from_canvas = (x) => Math.floor(x / cell_size);
+    const to_canvas  = (i) => i * cell_size;
+    const gof = canvas_to_gof(canvas, cell_size);
+    const draw_cell = (val, i, j) => {
+        let color = colors[val];
+        ctx.fillStyle = color;
+        ctx.fillRect(to_canvas(i), to_canvas(j), cell_size, cell_size);
+    }
+        
+    
+    this.toggle = (i,j) => {
+        let val = gof.toggle(i,j);
+        draw_cell(val, i, j);
+    }
+    
+    this.show = () => gof.cells.for_each( draw_cell );
+    this.next = () => { 
+        gof.next(); 
+        this.show();
+    }
+    
+    this.clear = () => {
+	    gof.clear();
+	    this.show();
+    }
+    this.save = () => saved[this.generation] = gof;
+    this.saved_pattern = undefined;
+    this.handle = (e_down_coords, e_up_coords) => {
+        const i0 = from_canvas(e_down_coords[0]);
+        const j0 = from_canvas(e_down_coords[1]);
+        const i1 = from_canvas(e_up_coords[0]);
+        const j1 = from_canvas(e_up_coords[1]);
 
-var rate_slider = document.getElementById("rate-slider");
-rate_slider.onchange = function() {rate_cont.set_rate(this.value)}
+        // if the events occur in the same pixel, toggle it
+        if (i0 === i1 && j0 === j1) {
+            this.toggle(i0, j0);
+        }
 
+        else {
+            this.saved_pattern = gof.cells.rect(i0, j0, i1, j1);
+        }
+    }
+    this.handle_merge = function(x,y) {
+        if (this.saved_pattern) {
+            gof.cells.merge(this.saved_pattern, from_canvas(x), from_canvas(y));
+            this.show();
+        }
+    }
+}
+    
 
