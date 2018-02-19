6398eda0a1022fff251826ed5398093e0fa46b9d
Stefan Vukicevic <stefanvuk17@gmail.com>
2018-02-18 23:11:36 -0800

    Use Canvas_set instead of explicitely managing multiple canvases. Add pattern copyting and loading.

--- js/state.js
+++ js/state.js
@@ -3,7 +3,9 @@ const state = {
     step_on: false,
     grid_on: false,
     cell_size : 10,
-    pattern : undefined,
+    //if you change this, look to change the initialization of the pattern canvas as well
+    //currently the pattern canvas is initialized witha cell_size of 10 which might be too small
+    pattern : newEmptyGameState(30, 30), 
     gamestate : undefined,
     rule : undefined,
     
@@ -70,12 +72,18 @@ function set_rule(new_rule) {
     state.notify("rule");
 }
 
+function set_pattern(new_gamestate) {
+    state.pattern = new_gamestate;
+    state.notify("pattern");
+}
+function get_pattern() {
+    return state.pattern;
+}
 
-
-
+///
+const gol_canvas_id = "#gol-canvas";
 ///Get Canvases
-const grid_canvas = $("#grid-canvas").get(0);
-const gol_canvas = $("#gol-canvas").get(0);
+const gol_canvas = $(gol_canvas_id).get(0);
 
 //Set initial game state: the maximum zoom size is 5
 state.gamestate = GameStateFromCanvas(gol_canvas, 5);
@@ -84,10 +92,42 @@ state.gamestate = GameStateFromCanvas(gol_canvas, 5);
 state.rule = classic_rule;
 
 //Create and Initialize main canvas set
-console.log("making main_canvas");
-const main_canvas = new Canvas_set(gol_canvas, grid_canvas, 10);
-console.log("ok, initializing it");
-main_canvas.initialize(state.gamestate);
+const main_c_set = new Canvas_set("#gol-canvas", 10);
+main_c_set.initialize(state.gamestate, 10, get_gamestate, set_gamestate);
+
+state.add_observer('gamestate', main_c_set);
+state.add_observer('grid_on', main_c_set);
+state.add_observer('cell_size', main_c_set);
+
+//Default behavior for clicks is to toggle the underlying cell.
+//We override it here so that shift-clicking will copy in the currently saved pattern
+$(main_c_set.over_canvas).unbind('click');
+$(main_c_set.over_canvas).click( function(e) {
+    if (e.shiftKey)  
+	main_c_set.merge(state.pattern, e.offsetX, e.offsetY);
+    else
+	main_c_set.toggle(e.offsetX, e.offsetY);
+});
+
+	
+main_c_set.update = function(property) {
+    switch(property) {
+    case 'gamestate':
+	this.project();
+	break;
+    case 'grid_on':
+	this.toggle_grid();
+	break;
+    case 'cell_size':
+	this.coordinate_handler.resize(state.cell_size);
+	this.projector.resize(this.coordinate_handler);
+	this.grid.resize(state.cell_size);
+	break;
+    }
+}
+
+
+
 
 ///Create the stepper which controls the rate the game of life evolves at
 const stepper = new RateController(state.rate, () =>
@@ -104,3 +144,27 @@ stepper.update = function(property) {
 	break;
     }
 }
+
+///Create canvas set for the pattern canvas
+const pattern_canvas_id = "#pattern_canvas";
+const pattern_c_set = new Canvas_set(pattern_canvas_id, 10); //hardcoded 10
+pattern_c_set.initialize(state.pattern, 10, get_pattern, set_pattern);
+pattern_c_set.toggle_grid();
+
+state.add_observer('pattern', pattern_c_set);
+
+pattern_c_set.update = function(property) {
+    switch (property) {
+    case 'pattern':
+	const csize = auto_cell_size(pattern_c_set.under_canvas, state.pattern);
+	this.change_gamestate(get_pattern(), csize);
+	this.grid.resize(csize);
+	break;
+    }
+}
+
+
+
+//For the time being, initialize the pattern canvas and the RLE text area w/ a backrake
+$("#rle_textarea").val(patterns.backrake_1);
+set_pattern(rle.decode(patterns.backrake_1).gamestate);
