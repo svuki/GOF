243fc25f1598b59af6b513d1aa9daa3f98ee5328
Stefan Vukicevic <stefanvuk17@gmail.com>
2018-02-21 11:51:28 -0800

    Use expliciting setting for state object instead of wrapper functions.

--- js/control.js
+++ js/control.js
@@ -1,20 +1,20 @@
 //buttons and sliders
 $("#toggle_grid_btn").click( () => toggle_grid() );
-$("#soup_btn").click( () => set_gamestate(state.gamestate.soup()) );
-$("#zoom-slider").on( 'input', function() { set_cell_size(this.value) });
-$("#reset_btn").click( () => set_gamestate(state.gamestate.clear()) );
+$("#soup_btn").click( () => state.gamestate = state.gamestate.soup() );
+$("#zoom-slider").on( 'input', function() {state.cell_size = this.value });
+$("#reset_btn").click( () => state.gamestate = state.gamestate.clear() );
 $("#toggle_step_btn").click( () => toggle_step() );
 $("#rate_slider").on( 'input',
 		      function() {
-			  set_rate(this.value)});
+			  state.rate = this.value});
 
 
-const rle_textarea = $("#rle_textarea");
+const rle_textarea = $(rle_textarea_id);
 
 $("#rle_submit_btn").click( function() {
     const rle_text = rle_textarea.val();
     const result = rle.decode(rle_text);
-    set_pattern(result.gamestate);
+    state.pattern = result.gamestate;
 });
 $("#rle_generate_btn").click( function() {
     const rle_str = rle.encode(state.gamestate, state.rule);
